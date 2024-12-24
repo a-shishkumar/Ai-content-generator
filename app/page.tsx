7c89b39f3746ce36f6f2a5e2148c5cc9f920a466
Ashish <ashishkr.0727@gmail.com>
2024-12-24 17:15:31 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+// import { Button } from "@/components/ui/button";
+// import Image from "next/image";
+// import Dashboard from "./dashboard/page";
+
+// export default function Home() {
+//   return (
+//    <>
+//    {/* <Button>subscribe</Button> */}
+//    {/* <Dashboard/> */}
+//    </>
+//   );
+// }
+
+
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-   <>
-   <Button>subscribe</Button>
-   </>
-  );
+  // Redirect to /dashboard
+  redirect('/dashboard');
+
+  return null; // This component won't render because of the redirect.
 }
